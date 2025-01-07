273a801cbed8e5ba44d8188185550d1c9b7f9b0d
Rohit Biseswar <r.k.biseswar@student.tudelft.nl>
2025-01-07 01:55:28 +0100

    Updates

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="pb-3 border-b-2 border-b-dark dark:border-b-light">
-        <a class="text-white no-underline hover:text-gray-300" href="/">
+        <a class="text-dark dark:text-white no-underline hover:text-dark dark:hover:text-white hover:opacity-80" href="/">
             <h1 class="text-3xl font-bold italic">{{ $t('title')}}</h1>
         </a>
     </div>
