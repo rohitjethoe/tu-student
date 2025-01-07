012a1d6d9aec1c2c0d2f10ba400d2de5262d0d25
Rohit Jethoe <69147709+rohitjethoe@users.noreply.github.com>
2025-01-07 23:33:52 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="py-4 pt-20">
-        Copyright {{ new Date().getFullYear() }} &copy; www.tustudent.blog
+    <div class="py-4 pt-20 text-xs">
+        <a href="#">about www.tustudent.blog</a>
     </div>
 </template>
