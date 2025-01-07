d1289162031ab9d50e6b5cd52652870ecab0b873
Rohit Jethoe <69147709+rohitjethoe@users.noreply.github.com>
2025-01-07 23:29:58 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,5 @@
 <template>
     <div class="py-4 pt-20">
-        <a class="flex items-end gap-3 underline" href="/substack">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ff6719" class="bi bi-substack" viewBox="0 0 16 16">
-              <path d="M15 3.604H1v1.891h14v-1.89ZM1 7.208V16l7-3.926L15 16V7.208zM15 0H1v1.89h14z"/>
-            </svg>
-            <p>{{ $t('substack') }}</p>
-        </a>
+        www.tustudent.blog &copy; {{ new Date().getFullYear() }} 
     </div>
 </template>
